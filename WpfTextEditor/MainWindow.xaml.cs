ca60a2633225065f7f4a33622847ccf988393e50
Aleksey <burovalexprogrammer@gmail.com>
2020-09-09 21:26:19 +0500

    Сохранение и открытие файла

--- WpfTextEditor/MainWindow.xaml.cs
+++ WpfTextEditor/MainWindow.xaml.cs
@@ -34,30 +34,57 @@ namespace WpfTextEditor
             if (menuItem == null) return;
             //Выбор шрифта
             if (menuItem.Tag?.Equals("FontButton") ?? false)
-            {
                 mainTextArea.FontFamily = menuItem.FontFamily;
-            }
+            //Новый файл
+            if (menuItem == miNewFile)
+                mainTextArea.Text = "";
+
             //Сохранение файла
             if (menuItem == miSaveFile)
             {
-                var saveDialog = new SaveFileDialog();
+                var saveDialog = new SaveFileDialog() { Filter = "Текстовый файл .txt|*.txt" };
                 saveDialog.ShowDialog();
-
-
+                if (saveDialog.FileName == string.Empty) {
+                    MessageBox.Show("Файл не был сохранен", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return; 
+                }
+                using (Stream s = File.Open(saveDialog.FileName, FileMode.OpenOrCreate))
+                    using (StreamWriter sw = new StreamWriter(s))
+                        sw.Write(mainTextArea.Text);
             }
+
             //Открытие файла
             if (menuItem == miOpenFile)
             {
                 var openDialog = new OpenFileDialog();
                 openDialog.ShowDialog();
+
+                if (openDialog.FileName == string.Empty)
+                {
+                    MessageBox.Show("Файл не был открыт", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                Stream s;
+                if ((s = openDialog.OpenFile())!= null)
+                {
+                    var fileName = openDialog.FileName;
+                    var fileText = File.ReadAllText(fileName);
+                    mainTextArea.Text = fileText;
+                }
             }
+
             //Выход
             if (menuItem == miQuit) this.Close();
         }
 
         private void Toolbar_Click(object sender, RoutedEventArgs e)
         {
-
+            //Сохранить файл
+            if (sender == tbSave)
+                Menu_Click(miSaveFile, null);
+            if (sender == tbOpen)
+                Menu_Click(miOpenFile, null);
         }
 
         private void selectFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
