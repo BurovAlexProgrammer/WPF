f7bf9d1e69f2908ef37a4bae5a55718bd562294b
Aleksey <burovalexprogrammer@gmail.com>
2020-09-09 10:46:30 +0500

    Меню
    Создал меню и реагирование

--- WpfTextEditor/MainWindow.xaml.cs
+++ WpfTextEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,10 +26,36 @@ namespace WpfTextEditor
             InitializeComponent();
         }
 
+        //Клик по элементу меню
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
-           // if (sender == miQuit) Quit();
-            //if (sender == miSaveFile) miSaveFile();
+            var menuItem = sender as MenuItem;
+            if (menuItem == null) return;
+            //Выбор шрифта
+            if (menuItem.Tag?.Equals("FontButton") ?? false)
+            {
+                mainWindow.FontFamily = menuItem.FontFamily;
+                var c  = mainMenu.Items.Cast<MenuItem>().Count();
+            }
+            //Сохранение файла
+            if (menuItem == miSaveFile)
+            {
+                var saveDialog = new SaveFileDialog();
+                saveDialog.ShowDialog();
+            }
+            //Открытие файла
+            if (menuItem == miOpenFile)
+            {
+                var openDialog = new OpenFileDialog();
+                openDialog.ShowDialog();
+            }
+            //Выход
+            if (menuItem == miQuit) this.Close();
+        }
+
+        private void Toolbar_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
